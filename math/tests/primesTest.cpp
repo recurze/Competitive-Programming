c233afe93a5da6464afca63b33ef719a4359dbd8
recurze <vrahul24999@gmail.com>
2018-12-25 21:36:37 +0530

    Add tests for factorization and divisors

--- math/tests/primesTest.cpp
+++ math/tests/primesTest.cpp
@@ -2,7 +2,7 @@
  * File              : primesTest.cpp
  * Author            : recurze
  * Date              : 17:07 25.12.2018
- * Last Modified Date: 17:41 25.12.2018
+ * Last Modified Date: 21:34 25.12.2018
  */
 
 #include "primes.cpp"
@@ -11,13 +11,47 @@
 #include <cmath>
 #include <iostream>
 
-int main() {
+void test_prime() {
     sieve(1010101);
     for (int i = 1; i < 1010101; ++i) {
-        assert (prime[i] == isPrime(i));
         if (prime[i]) {
-            std::cout<<i<<'\n';
+            std::cerr<<i<<'\n';
         }
+        assert (prime[i] == isPrime(i));
     }
+}
+
+void test_primefactors() {
+    int n, m, j;
+    for (int i = 0; i < 100000; ++i) {
+        n = rand();
+        auto pfc = getPrimefactorsCount(n);
+        auto pf = getDistinctPrimeFactors(n);
+        m = 1, j = 0;
+        for (auto& fc: pfc) {
+            for (int c = 0; c < fc.second; ++c) {
+                assert(isPrime(fc.first));
+                m *= (fc.first);
+            }
+            assert(pf[j++] == fc.first);
+        }
+
+        std::cerr<<n<<'\n';
+        assert (n == m);
+    }
+}
+
+void test_divisors() {
+    auto d = getDivisors(110);
+    for (int i: d) {
+        std::cerr<<i<<' ';
+    }
+}
+
+int main() {
+    test_prime();
+    srand(time(0));
+    test_primefactors();
+    test_divisors();
     return 0;
 }
