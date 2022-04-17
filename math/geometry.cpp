64a5bf8c88d30ff16df6628acde44ba9c3ab54ad
recurze <vrahul24999@gmail.com>
2022-04-17 22:47:11 +0200

    Write vector and mod as classes

--- math/geometry.cpp
+++ math/geometry.cpp
@@ -1,59 +1,41 @@
-#include <cmath>
-
 template <typename T>
-struct Vector {
-    T x, y;
+class Vector {
+public:
     Vector() { }
+    Vector(const Vector& a) : x(a.x), y(a.y) { }
+    Vector(const T& _x, const T& _y) : x(_x), y(_y) { }
+
+    Vector& operator=(const Vector& b) { x = b.x; y = b.y; return *this; }
+
+    constexpr std::pair<T, T> operator()() const { return {x, y}; }
+
+    Vector& operator-=(const Vector& b) { x -= b.x; y -= b.y; return *this; }
+    Vector& operator+=(const Vector& b) { x += b.x; y += b.y; return *this; }
+
+    template <typename S, typename std::enable_if_t< std::is_integral_v<S>, bool> = true>
+    Vector& operator*=(const S& s) { x *= s; y *= s; return *this; }
+
+    friend double norm(const Vector<T>& a) {
+        return std::sqrt(a.x*a.x + a.y*a.y);
+    }
+
+    friend Vector operator-(Vector a, const Vector& b) { return a -= b; }
+    friend Vector operator+(Vector a, const Vector& b) { return a += b; }
+
+    friend std::int64_t   dot(const Vector& a, const Vector& b) { return a.x*b.x + a.y*b.y; }
+    friend std::int64_t cross(const Vector& a, const Vector& b) { return a.x*b.y - a.y*b.x; }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector& a) { return os << a.x << ' ' << a.y; }
+private:
+    T x, y;
 };
-
-template <typename T>
-Vector<T> add(const Vector<T>& a, const Vector<T>& b) {
-    return Vector<T>{ .x = a.x + b.x, .y = a.y + b.y };
-}
-
-template <typename T>
-Vector<T> sub(const Vector<T>& a, const Vector<T>& b) {
-    return Vector<T>{ .x = a.x - b.x, .y = a.y - b.y };
-}
-
-template <typename T>
-void add_to(Vector<T>& a, const Vector<T>& b) {
-    a.x += b.x;
-    a.y += b.y;
-}
-
-template <typename T>
-void sub_from(Vector<T>& a, const Vector<T>& b) {
-    a.x -= b.x;
-    a.y -= b.y;
-}
-
-template <typename T, typename S>
-void scale(Vector<T>& a, const S& s) {
-    a.x *= s;
-    a.y *= s;
-}
-
-template <typename T>
-double norm(const Vector<T>& a) {
-    return std::sqrt(x*x + y*y);
-}
-
-template <typename T>
-T dot(const Vector<T>& a, const Vector<T>& b) {
-    return a.x*b.x + a.y*b.y;
-}
-
-template <typename T>
-T cross(const Vector<T>& a, const Vector<T>& b) {
-    return a.x*b.y - a.y*b.x;
-}
 
 template <typename T>
 double areaOfPolygon(const std::vector< Vector<T> >& points) {
     double area = 0;
-    for (std::size_t i = 1; i < points.size() - 1; ++i) {
-        area += cross(sub(points[i], points[0]), sub(points[i + 1], points[0]));
+    auto num_points = points.size();
+    for (decltype(num_points) i = 1; i < num_points - 1; ++i) {
+        area += cross(points[i] - points[0], points[i + 1] - points[0]);
     }
     return std::abs(area)/2;
 }
--- math/modarith.cpp
+++ math/modarith.cpp
@@ -1,65 +1,83 @@
-template <typename T, typename E>
-T exp(T a, E b, const T& p = MD) {
-    T res = 1;
-    for (; b; b >>= 1, a = mul(a, a)) if (b&1) {
-        res = mul(res, a);
+#include <ostream>
+#include <type_traits>
+#include <cstdint>
+constexpr int MD = 10;
+template <typename T, typename std::enable_if_t< std::is_signed_v<T>, bool> = true>
+class Mod {
+public:
+    Mod() : value(0), m(MD) {}
+    Mod(const Mod& a) : value(a.value), m(a.m) {}
+
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    Mod(const U& n, const T& _m = MD) : value(normalize(n, _m)), m(_m) {}
+
+    Mod& operator=(const Mod& b) { value = b.value; return *this; }
+
+    constexpr T operator()() const { return value; }
+
+    Mod& operator-=(const Mod& b) { if ((value -= b.value) <   0) value += m; return *this; }
+    Mod& operator+=(const Mod& b) { if ((value += b.value) >= m) value -= m; return *this; }
+    Mod& operator/=(const Mod& b) { return *this *= inverse(b); }
+    Mod& operator*=(const Mod& b) {
+        // assuming m*m fits in int64_t
+        value = static_cast<T>(static_cast<std::int64_t>(value)*b.value % m);
+        return *this;
     }
-    return res;
-}
 
-// Fermat's little theorem: a^p-1 = 1 (mod p) for 0 < a < p
-template <typename T>
-T inverse(const T& a, const T& p = MD) {
-    return exp(a, MD - 2, MD);
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    Mod& operator-=(const U& b) { return *this -= Mod(b); }
 
-template <typename T>
-void normalize(T& a, const T& p = MD) {
-    a %= p;
-    if (a < 0) a += p;
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    Mod& operator+=(const U& b) { return *this += Mod(b); }
 
-template <typename T>
-void add(const T& a, const T& b, const T& p = MD) {
-    T res = a + b;
-    return res >= p ? res - p : res;
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    Mod& operator*=(const U& b) { return *this *= Mod(b); }
 
-template <typename T>
-void sub(const T& a, const T& b, const T& p = MD) {
-    T res = a - b;
-    return res < 0 ? res + p : res;
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    Mod& operator/=(const U& b) { return *this /= Mod(b); }
 
-template <typename T>
-void add_to(T& a, const T& b, const T& p = MD) {
-    a += b;
-    if (a >= p) a -= p;
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    static T normalize(U n, const T& m) {
+        if (n < 0 or n >= m) n %= m;
+        if (n < 0) n += m;
+        return static_cast<T>(n);
+    }
 
-template <typename T>
-void sub_from(T& a, const T& b, const T& p = MD) {
-    a -= b;
-    if (a < 0) a += p;
-}
+    friend Mod operator-(Mod a, const Mod& b) { return a -= b; }
+    friend Mod operator+(Mod a, const Mod& b) { return a += b; }
+    friend Mod operator*(Mod a, const Mod& b) { return a *= b; }
+    friend Mod operator/(Mod a, const Mod& b) { return a /= b; }
 
-void mul(int a, int b, int p = MD) {
-    return std::static_cast<int>(std::static_cast<long long int>(a)*b % p);
-}
+    template <typename U, typename std::enable_if_t< std::is_integral_v<U>, bool> = true>
+    friend Mod pow(Mod a, U b) {
+        assert(b >= 0);
+        Mod res(1);
+        for (; b; b >>= 1, a *= a) {
+            if (b&1) {
+                res *= a;
+            }
+        }
+        return res;
+    }
 
-void div(int a, int b, int p = MD) {
-    return mul(a, inverse(b, p), p);
-}
+    // assuming m is prime; otherwise use extended_gcd
+    friend Mod inverse(Mod a) { return pow(a, a.m - 2); }
 
-// Properties:
-//   - Mod inverse exists if gcd(a, n) = 1
-//   - Euler's: a^phi(n) = 1 (mod n), where phi(n) is euler's totient
-//   - a = b (mod phi(n)) => k^a = k^b (mod n) given gcd(k, n) = 1
-//   - Wilson's: p is prime <=> (p - 1)! = -1 (mod p)
-//   - CRT: x = a (mod m) and x = b (mod n) and gcd(m, n) = 1 then
-//       x = bm^-1m + an^-1n (mod mn), where m^-1 = modulo inverse of m with n
-//   - Lagrange's: f(x) = 0 (mod p) and f(x) = a0x^n + ... + an
-//       and a0 != 0 (mod p), then f(x) has n roots
-//   - Quadratic residue: a is QR mod n if x^2 = a (mod n)
-//   - Euler's criterion for QR: p is odd rpime and p !| a then a is QR iff
-//       a^(p-1)/2 = 1 (mod p)
+    friend std::ostream& operator<<(std::ostream& os, const Mod& a) { return os << a(); }
+private:
+    T value;
+    T m;
+
+    // Properties:
+    //   - Mod inverse exists if gcd(a, n) = 1
+    //   - Euler's: a^phi(n) = 1 (mod n), where phi(n) is euler's totient
+    //   - a = b (mod phi(n)) => k^a = k^b (mod n) given gcd(k, n) = 1
+    //   - Wilson's: p is prime <=> (p - 1)! = -1 (mod p)
+    //   - CRT: x = a (mod m) and x = b (mod n) and gcd(m, n) = 1 then
+    //       x = bm^-1m + an^-1n (mod mn), where m^-1 = modulo inverse of m wrt n
+    //   - Lagrange's: f(x) = 0 (mod p) and f(x) = a0x^n + ... + an
+    //       and a0 != 0 (mod p), then f(x) has n roots
+    //   - Quadratic residue: a is QR mod n if x^2 = a (mod n)
+    //   - Euler's criterion for QR: p is odd prime and p !| a then a is QR iff
+    //       a^(p-1)/2 = 1 (mod p)
+};
