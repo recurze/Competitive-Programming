24d74f86c481015dda55b07753d5269be10dd72b
recurze <vrahul24999@gmail.com>
2022-04-17 22:47:41 +0200

    Write nCr using mod class

--- math/pnc.cpp
+++ math/pnc.cpp
@@ -1,63 +1,63 @@
-#include <algorithm>
-
 const int N = 1000;
+using mint = Mod<int>;
 
-std::array<std::array<int, N>, N> c;
+std::array<std::array<mint, N>, N> c;
 void buildCTable(int n) {
     for (int i = 0; i <= n; ++i) {
         c[i][0] = c[i][i] = 1;
         for (int j = 1; j < i; ++j) {
-            add_to(c[i][j], c[i - 1][j]);
-            add_to(c[i][j], c[i - 1][j - 1]);
+            c[i][j] += c[i - 1][j];
+            c[i][j] += c[i - 1][j - 1];
         }
     }
 }
 
-int nCrp(int n, int r, int p = MD) {
-    if (r > n/2) r = n - r;
+mint nCrp(int n, int r, int p) {
+    r = std::min(r, n - r);
 
-    int res = 1;
+    mint res(1, p);
     for (int i = 1; i <= r; ++i) {
-        res = mul(res, div(n - r + i, i, p), p);
+        res *= (n - r + i);
+        res /= i;
     }
     return res;
 }
 
-int nCrModp(int n, int r, int p = MD) {
+mint nCrModp(int n, int r, int p = MD) {
     if (r == 0) return 1;
     if (n < p) return nCrp(n, r, p);
 
-    int res = 1;
+    mint res(1, p);
     for (; r; n /= p, r /= p) {
-        res = mul(res, nCrModp(n%p, r%p, p), p);
+        res *= nCrModp(n%p, r%p, p);
     }
     return res;
 }
 
-int nCrp_CRT(int n, int r, int p) {
-    int res = 0;
-    for (auto f: get_prime_factors(p)) {
-        int a = nCrModp(n, r, f);
-        int z = p/f;
-        int y = inverse(z, f);
-        int w = mul(z, y, p);
-        add(res, mul(a, w, p));
+int nCrp_CRT(int n, int r, int m) {
+    mint res(0, m);
+    for (auto [f, _]: get_prime_factors_count(m)) {
+        auto a = nCrModp(n, r, f);
+        auto z = mint(m/f, f);
+        auto y = inverse(z);
+        auto w = z*y;
+        res += a*w;
     }
-    return res;
+    return res();
 }
 
 // This function is exhibit-only; use std::next_permutation of <algorithm>
-void perm() {
-    int c[n] = {0}, i = 0;
-    //do_something();
-    while (i < n) {
-        if (c[i] < i) {
-            (i&1) ? std::swap(a[i], a[c[i]]) : std::swap(a[i], a[0]);
-            //do_something();
-            ++c[i];
-            i = 0;
-        } else {
-            c[i++] = 0;
-        }
-    }
-}
+//void perm() {
+//    int c[n] = {0}, i = 0;
+//    //do_something();
+//    while (i < n) {
+//        if (c[i] < i) {
+//            (i&1) ? std::swap(a[i], a[c[i]]) : std::swap(a[i], a[0]);
+//            //do_something();
+//            ++c[i];
+//            i = 0;
+//        } else {
+//            c[i++] = 0;
+//        }
+//    }
+//}
