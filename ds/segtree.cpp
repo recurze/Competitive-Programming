d8149156c417b9f2991b580fb7f92ce4895d6e7e
recurze <vrahul24999@gmail.com>
2022-04-18 19:15:50 +0200

    Update segtree.cpp
    I'd like to add more meaningful commit messages but I'm just cleaning up for now

--- ds/segtree.cpp
+++ ds/segtree.cpp
@@ -3,16 +3,17 @@
 
 template <typename T>
 class SegmentTree {
+#define mid(l, r) ((l) + ((r) - (l))>>1)
 #define lchild(i) (i << 1)
-#define rchild(i) (i << 1 | 1)
+#define rchild(i) ((i << 1) | 1)
 public:
-    SegmentTree(const std::vector<T>& a) : n((int) a.size()), a(a), t(std::vector<Segment>(n << 2)) {
+    SegmentTree(const std::vector<T>& _a) : n((int) _a.size()), a(_a), t(std::vector<Node>(n << 2)) {
         build(0, n);
     }
 
     T query(int l, int r) {
         assert(l < r);
-        return _query(l, r, 1, 0, n).val;
+        return _query(l, r, 1, 0, n);
     }
 
     void update(int pos, const T& new_value) {
@@ -22,53 +23,65 @@ public:
 
     template <typename C>
     int find_first(int l, int r, const C& test) {
-        return _find_first(l, r, test, 1, 0, n);
+        assert(l < r);
+        return _find_first(l, r, test);
     }
 private:
-    struct Segment {
+    struct Node {
         T val;
+        Node() { }
+        Node(const T& v) : val(v) { }
+        Node(const Node& node) : val(node.val) { }
+
+        Node& operator=(const Node& node) {
+            val = node.val;
+            return *this;
+        }
+
+        Node& operator+=(const Node& node) {
+            val = std::max(val, node.val);
+            return *this;
+        }
+
+        operator T() const { return val; }
+
+        friend Node operator+(Node node1, const Node& node2) { return node1 += node2; }
     };
 
     const int n;
     std::vector<T> a;
-    std::vector<Segment> t;
+    std::vector<Node> t;
 
-    static Segment combine(const Segment& a, const Segment& b) {
-        return Segment{std::max(a.val, b.val)};
-    }
-
-    void push_up(int i) {
-        t[i] = combine(t[lchild(i)], t[rchild(i)]);
-    }
+    void push_up(int i) { t[i] = t[lchild(i)] + t[rchild(i)]; }
 
     void build(int l, int r, int i = 1) {
         if (r - l == 1) {
-            t[i] = Segment{a[l]};
+            t[i] = Node(a[l]);
             return;
         }
-        auto c = l + (r - l)/2;
+        auto c = mid(l, r);
         build(l, c, lchild(i));
         build(c, r, rchild(i));
         push_up(i);
     }
 
-    Segment _query(int l, int r, int i, int tl, int tr) {
+    Node _query(int l, int r, int i, int tl, int tr) {
         assert(tl <= l and l < r and r <= tr);
         if (l == tl and r == tr) return t[i];
-        auto tc = tl + (tr - tl)/2;
+        auto tc = mid(tl, tr);
         if (r <= tc) return _query(l, r, lchild(i), tl, tc);
         if (l >= tc) return _query(l, r, rchild(i), tc, tr);
-        return combine(_query(l, tc, lchild(i), tl, tc), _query(tc, r, rchild(i), tc, tr));
+        return _query(l, tc, lchild(i), tl, tc) + _query(tc, r, rchild(i), tc, tr);
     }
 
     void _update(int pos, const T& new_value, int i, int tl, int tr) {
         if (tr - tl == 1) {
             assert(pos == tl);
             a[pos] = new_value;
-            t[i] = Segment{a[pos]};
+            t[i] = Node(a[pos]);
             return;
         }
-        auto tc = tl + (tr - tl)/2;
+        auto tc = mid(tl, tr);
         if (pos < tc) {
             _update(pos, new_value, lchild(i), tl, tc);
         } else {
@@ -80,11 +93,11 @@ private:
     template <typename C>
     int _find_first(int l, int r, const C& test, int i, int tl, int tr) const {
         assert(tl <= l and l < r and r <= tr);
-        if (not test(t[i].val)) return n;
+        if (not test(t[i])) return n;
 
         if (tr - tl == 1) return tl;
 
-        auto tc = tl + (tr - tl)/2;
+        auto tc = mid(tl, tr);
         if (r <= tc) return _find_first(l, r, test, lchild(i), tl, tc);
         if (l >= tc) return _find_first(l, r, test, rchild(i), tc, tr);
 
@@ -95,20 +108,26 @@ private:
 
 template <typename T>
 class LazySegmentTree {
+#ifndef mid
+#define mid(l, r) ((l) + ((r) - (l))>>1)
 #define lchild(i) (i << 1)
 #define rchild(i) (i << 1 | 1)
+#endif
 public:
-    LazySegmentTree(const std::vector<T>& a) :
-        n((int) a.size()), a(a),
-        t(std::vector<Segment>(n << 2)),
-        lazy(std::vector<T>(n << 2, 0)),
+    LazySegmentTree(const std::vector<T>& _a) :
+        n((int) _a.size()), a(_a),
+        t(std::vector<Node>(n << 2)),
+#ifndef UNIFORM_APPLY
+        lazy(std::vector<T>(n << 2, 0)) {
+#else
         marked(std::vector<bool>(n << 2, false)) {
+#endif
         build(0, n);
     }
 
     T query(int l, int r) {
         assert(l < r);
-        return _query(l, r, 1, 0, n).val;
+        return _query(l, r, 1, 0, n);
     }
 
     void apply_range(int l, int r, const T& v) {
@@ -116,27 +135,50 @@ public:
         _apply_range(l, r, v, 1, 0, n);
     }
 private:
-    struct Segment {
+    struct Node {
         T val;
+        Node() { }
+        Node(const T& v) : val(v) { }
+        Node(const Node& node) : val(node.val) { }
+
+        Node& operator=(const Node& node) {
+            val = node.val;
+            return *this;
+        }
+
+        Node& operator+=(const Node& node) {
+            val = std::max(val, node.val);
+            return *this;
+        }
+
+        operator T() const { return val; }
+
+        friend Node operator+(Node node1, const Node& node2) { return node1 += node2; }
     };
 
     const int n;
     std::vector<T> a;
-    std::vector<Segment> t;
+    std::vector<Node> t;
+
+#ifndef UNIFORM_APPLY
     std::vector<T> lazy;
+
+    void apply(int i, const T& v) {
+        t[i].val += v; lazy[i] += v;
+    }
+
+    void push_down(int i) {
+        if (lazy[i] != 0) {
+            apply(lchild(i), lazy[i]);
+            apply(rchild(i), lazy[i]);
+            lazy[i] = 0;
+        }
+    }
+#else
     std::vector<bool> marked;
 
     void apply(int i, const T& v) {
-        //t[i].val = v; marked[i] = true;
-        //t[i].val += v; lazy[i] += v;
-    }
-
-    static Segment combine(const Segment& a, const Segment& b) {
-        return Segment{std::max(a.val, b.val)};
-    }
-
-    void push_up(int i) {
-        t[i] = combine(t[lchild(i)], t[rchild(i)]);
+        t[i].val = v; marked[i] = true;
     }
 
     void push_down(int i) {
@@ -145,16 +187,14 @@ private:
             apply(rchild(i), t[i].val);
             marked[i] = false;
         }
-        if (lazy[i] != 0) {
-            apply(lchild(i), lazy[i]);
-            apply(rchild(i), lazy[i]);
-            lazy[i] = 0;
-        }
     }
+#endif
+
+    void push_up(int i) { t[i] = t[lchild(i)] + t[rchild(i)]; }
 
     void build(int l, int r, int i = 1) {
         if (r - l == 1) {
-            t[i] = Segment{a[l]};
+            t[i] = Node{a[l]};
             return;
         }
         auto c = l + (r - l)/2;
@@ -163,9 +203,11 @@ private:
         push_up(i);
     }
 
-    Segment _query(int l, int r, int i, int tl, int tr) {
+    Node _query(int l, int r, int i, int tl, int tr) {
         assert(tl <= l and l < r and r <= tr);
+#ifdef UNIFORM_APPLY
         if (marked[i]) return t[i];
+#endif
         if (l == tl and r == tr) return t[i];
         push_down(i);
         auto tc = tl + (tr - tl)/2;
